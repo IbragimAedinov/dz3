8730277275e478e99fb711f3561182f6122521a7
IbragimAedinov <ibragimae8@yandex.ru>
2024-05-08 21:14:36 +0300

    добавлен GET запрос по адресу /

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const req = require("express/lib/request");
+const res = require("express/lib/response");
 
 const app = express();
 const PORT = 3000;
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
 
 app.listen(PORT, () => {
   console.log(`Приложение запущено тут: http:  //localhost:${PORT}`);
