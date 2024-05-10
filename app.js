0d8144a9fc3f5d245ec0d49731f779e85fa4da19
IbragimAedinov <ibragimae8@yandex.ru>
2024-05-10 19:17:13 +0300

    добавлена настройка cors

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const gamesRouter = require("./routes/games");
 const app = express();
 const PORT = 3000;
 app.use(
+  cors,
   bodyParser.json(),
   express.static(path.join(__dirname, "public")),
   mainRouter,
