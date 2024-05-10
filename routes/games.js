35326113fd209ebaee5ce1860cfd122f5d256b0a
IbragimAedinov <ibragimae8@yandex.ru>
2024-05-10 19:12:38 +0300

    доработки проекта

--- routes/games.js
+++ routes/games.js
@@ -1,8 +1,30 @@
-const { deleteGame, getAllGames, addGame } = require("../controllers/game");
+const { sendAllGames, sendUpdatedGames } = require("../controllers/game");
+const {
+  getAllGames,
+  checkisTitleInAray,
+  updateGamesArray,
+  updateGamesFile,
+  findGamesById,
+  deleteGame,
+} = require("../middlewares/games");
 
 const gamesRouter = require("express").Router();
 
-gamesRouter.get("/games", getAllGames);
-gamesRouter.delete("/games/ :id ", deleteGame);
-gamesRouter.post("/games", addGame);
+gamesRouter.get("/games", getAllGames, sendAllGames);
+gamesRouter.delete(
+  "/games/ :id ",
+  getAllGames,
+  updateGamesFile,
+  sendUpdatedGames,
+  findGamesById,
+  deleteGame
+);
+gamesRouter.post(
+  "/games",
+  getAllGames,
+  checkisTitleInAray,
+  updateGamesArray,
+  updateGamesFile,
+  sendUpdatedGames
+);
 module.exports = gamesRouter;
