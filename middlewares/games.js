6a1bbdb227fbb210e994498fe3bdcd245c309f59
IbragimAedinov <149922141+IbragimAedinov@users.noreply.github.com>
2024-05-16 21:53:45 +0300

    Update games.js

--- middlewares/games.js
+++ middlewares/games.js
@@ -1,22 +1,25 @@
-const { readData, writeData } = require("../utils/data/parse");
-const getAllGames = async (req, res) => {
-  const games = await readData("/data/games.json");
+const { readData, writeData } = require("../utils/data");
+
+const getAllGames = async (req, res, next) => {
+  const games = await readData("./data/games.json");
   if (!games) {
     res.status(400);
     res.send({
       status: "error",
-      message: "Нет игр в базе данных. Добавьте игру",
+      message: "Нет игр в базе данных. Добавь игру.",
     });
     return;
   }
   req.games = games;
   next();
 };
-const checkisTitleInAray = async (req, res, next) => {
+
+const checkIsTitleInArray = (req, res, next) => {
   req.isNew = !Boolean(req.games.find((item) => item.title === req.body.title));
   next();
 };
-const updateGamesArray = async (req, res, next) => {
+
+const updateGamesArray = (req, res, next) => {
   if (req.isNew) {
     const inArray = req.games.map((item) => Number(item.id));
     let maximalId;
@@ -25,40 +28,45 @@ const updateGamesArray = async (req, res, next) => {
     } else {
       maximalId = 0;
     }
-    req.updatedobject = {
+
+    req.updatedObject = {
       id: maximalId + 1,
       title: req.body.title,
       image: req.body.image,
       link: req.body.link,
       description: req.body.description,
     };
-
-    req.games = [...req.games, req.updatedobject];
+    req.games = [...req.games, req.updatedObject];
     next();
   } else {
     res.status(400);
-    res.send({ status: "error", message: "Nгpа таким именем уже есть." });
+    res.send({ status: "error", message: "Игра с таким именем уже есть." });
   }
 };
+
 const updateGamesFile = async (req, res, next) => {
   await writeData("./data/games.json", req.games);
   next();
 };
-const findGamesById = async (req, res, next) => {
+
+const findGameById = (req, res, next) => {
   const id = Number(req.params.id);
   req.game = req.games.find((item) => item.id === id);
   next();
 };
-const deleteGame = async (req, res, next) => {
-  const index = req.games.findIndex((item) => item.id === req.game.id);
+
+const deleteGame = (req, res, next) => {
+  const id = req.game.id;
+  const index = req.games.findIndex((item) => item.id === id);
   req.games.splice(index, 1);
   next();
 };
+
 module.exports = {
   getAllGames,
-  checkisTitleInAray,
+  checkIsTitleInArray,
   updateGamesArray,
   updateGamesFile,
+  findGameById,
   deleteGame,
-  findGamesById,
 };
